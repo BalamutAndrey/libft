17eb0102de51b7251fb58f8cb5b2be43148a976f
Earlean Boris <eboris@ox-p6.21-school.ru>
2019-09-28 15:03:27 +0300

    fix ft_strcmp & ft_strncmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: eboris <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 19:12:58 by eboris            #+#    #+#             */
-/*   Updated: 2019/09/19 08:57:15 by eboris           ###   ########.fr       */
+/*   Updated: 2019/09/28 15:00:48 by eboris           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,6 @@ int	ft_strcmp(const char *s1, const char *s2)
 	}
 	if ((s1[n] == '\0') && (s2[n] == '\0'))
 		return (0);
-	else if ((s1[n] == '\0') && (s2[n] != '\0'))
-		return (-1);
-	else if ((s1[n] != '\0') && (s2[n] == '\0'))
-		return (1);
 	else
 		return ((unsigned char)s1[n] - (unsigned char)s2[n]);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: eboris <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 19:37:01 by eboris            #+#    #+#             */
-/*   Updated: 2019/09/10 21:17:09 by eboris           ###   ########.fr       */
+/*   Updated: 2019/09/28 14:35:30 by eboris           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (((s1[i] == s2[i]) && (i < n)) && (s1[i] != '\0') && (s2[i] != '\0'))
+	while (((unsigned char)s1[i] == (unsigned char)s2[i]) && (i < n)
+			&& ((unsigned char)s1[i] != '\0') && ((unsigned char)s2[i] != '\0'))
 	{
 		i++;
 	}
 	if (i == n)
 		return (0);
-	else if ((s1[i] == '\0') && (s2[i] != '\0'))
-		return (-1);
-	else if ((s1[i] != '\0') && (s2[i] == '\0'))
-		return (1);
 	else
-		return (s1[i] - s2[i]);
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
