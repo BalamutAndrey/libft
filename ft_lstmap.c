3aa9b7eea239186c9d96e661c8396bd00955d67a
Earlean Boris <eboris@su-j6.21-school.ru>
2019-10-06 16:01:15 +0300

    fix ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: eboris <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 15:15:23 by eboris            #+#    #+#             */
-/*   Updated: 2019/09/18 15:39:47 by eboris           ###   ########.fr       */
+/*   Updated: 2019/10/06 15:58:48 by eboris           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,21 @@
 t_list	*ft_lstmap(t_list *lst, t_list *(*f) (t_list *elem))
 {
 	t_list	*first;
-	t_list	*prev;
 	t_list	*new;
 
-	first = NULL;
-	if (lst != NULL)
+	if ((!(lst)) || (!(f)))
+		return (NULL);
+	new = f(lst);
+	if (new == NULL)
+		return (NULL);
+	first = new;
+	while (lst->next != NULL)
 	{
-		first = ft_lstnew(lst->content, lst->content_size);
-		if (first == NULL)
-			return (NULL);
-		first = f(first);
-		prev = first;
 		lst = lst->next;
-	}
-	while (lst != NULL)
-	{
-		new = ft_lstnew(lst->content, lst->content_size);
-		if (new == NULL)
+		new->next = f(lst);
+		if (new->next == NULL)
 			return (NULL);
-		new = f(new);
-		prev->next = new;
-		prev = new;
-		lst = lst->next;
+		new = new->next;
 	}
 	return (first);
 }
